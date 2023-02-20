d68312fbca300e6ff397ad8d6f4b6d00d4aa3da6
gemtunde <gemtunde@gmail.com>
2023-02-20 13:26:46 +0100

    user schema

--- server/server.js
+++ server/server.js
@@ -9,10 +9,12 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+//retrieve data from server
 app.get('/', (req, res) => {
     res.json('hello amazon clone');
 });
 
+//send data from frontend to backend
 app.post('/', (req, res)=> {
     console.log(req.body)
 })

0e84ade01bbb727fd20e567a288350c665464a40
gemtunde <gemtunde@gmail.com>
2023-02-20 13:01:32 +0100

    server setup

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', (req, res) => {
+    res.json('hello amazon clone');
+});
+
+app.post('/', (req, res)=> {
+    console.log(req.body)
+})
+
+app.listen(3000, err => {
+    if(err){
+        console.log(err);
+    } else{
+        console.log("listening on port", 3000);
+    }
+})
+
+
+
+
+
+
+
+
+
+

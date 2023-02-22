7eaedfb57a70f8283456d0185dc775d0dd5f8b18
gemtunde <gemtunde@gmail.com>
2023-02-22 11:58:06 +0100

    product

--- server/routes/product.js
+++ server/routes/product.js
@@ -24,4 +24,68 @@ router.post('/products',upload.single("photo"), async (req, res)=> {
         })
     }
 });
+
+//get all products
+router.get("/products", async (req, res)=> {
+    try {
+        let products = await Product.find();
+
+        res.json({
+            success: true,
+            products: products
+        });
+    } catch (error) {
+        res.status(500).json({
+            status:false,
+            message: error.message
+        });
+    }
+});
+
+//single product
+router.get("/products/:id", async (req, res)=> {
+    try {
+        let product = await Product.findOne({_id : req.params.id});
+
+        res.json({
+            success :true,
+            product : product
+        });
+    } catch (error) {
+         res.status(500).json({
+            status:false,
+            message: error.message
+        });
+    }
+});
+
+//update product
+router.put("/products/:id", async (req, res)=> {
+    try {
+        let product = await Product.findOneAndUpdate({_id : req.params.id},
+        {
+            $set : {
+                title : req.body.title,
+                 description : req.body.description,  
+                stockQuantity : req.body.stockQuantity,
+                category : req.body.categoryID,
+                owner : req.body.ownerID
+            }
+
+        },
+        {upsert : true}
+        );
+
+        res.json({
+            success :true,
+            updatedProduct : product
+        });
+    } catch (error) {
+         res.status(500).json({
+            status:false,
+            message: error.message
+        });
+    }
+});
+
 module.exports = router ;
